dfda26704befb880f63db96dcde249ee99bbb19e
dannytszho <dannytszho@gmail.com>
2022-04-02 00:56:12 -0700

    Add and config storybook

--- src/Aware.js
+++ src/Aware.js
@@ -1,23 +1,42 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 // color list [blue, yellow, red, white]
 const COLORS = ["bg-yellow-400", "bg-blue-600", "bg-red-600"];
 
 const Aware = () => {
-  const [color, setColor] = useState(0);
-  const [number, setNumber] = useState(0);
+  const [color, setColor] = useState(null);
+  const [number, setNumber] = useState(null);
+  const [show, setShow] = useState(true);
 
-  useEffect(() => {
+  const handleClick = () => {
+    setShow(false);
     const interval = setInterval(() => {
       setColor((c) => (c === 2 ? 0 : c + 1));
       setNumber(Math.floor(Math.random() * 10 + 1));
     }, 1000);
     return () => clearInterval(interval);
-  }, []);
+  };
+
+  //  useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setColor((c) => (c === 2 ? 0 : c + 1));
+  //     setNumber(Math.floor(Math.random() * 10 + 1));
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return (
-    <div className={`h-screen p-2 mt-10 ${COLORS[color]}`}>
-      <h1 className="text-black text-9xl pt-56 text-center">{number}</h1>
+    <div>
+      <div className="w-screen h-screen absolute flex justify-center items-center">
+        {show ? (
+          <button className="border-black border-2 w-24" onClick={handleClick}>
+            Start Here!
+          </button>
+        ) : null}
+      </div>
+      <div className={`w-screen h-screen p-2 mt-10 ${COLORS[color]}`}>
+        <h1 className="text-black text-9xl pt-56 text-center">{number}</h1>
+      </div>
     </div>
   );
 };
